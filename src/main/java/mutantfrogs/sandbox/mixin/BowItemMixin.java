06529b60f45feb54ba04a048fe640c2e1fd17003
mutantfrogs <themutantfrogs@gmail.com>
2025-07-04 17:44:57 -0400

    fix: bow damage on use and bow durability initialization

--- src/main/java/mutantfrogs/sandbox/mixin/BowItemMixin.java
+++ src/main/java/mutantfrogs/sandbox/mixin/BowItemMixin.java
@@ -9,18 +9,27 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BowItem.class)
 public class BowItemMixin extends Item{
     public BowItemMixin(int i) {
         super(i);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void init(CallbackInfo ci){
         this.maxCount = 1;
         this.setMaxDamage(384);
     }
 
+    @Override
     public ItemStack use(ItemStack stack, World world, PlayerEntity user) {
         for (int index = 0; index < user.inventory.main.length; index++) {
             if (user.inventory.main[index] != null && user.inventory.main[index].itemId == Item.ARROW.id) {
+                stack.damage(1, user);
                 user.inventory.remove(Item.ARROW.id);
                 world.playSound(user, "random.bow", 1.0F, 1.0F / (random.nextFloat() * 0.4F + 0.8F));
                 if (!world.isRemote) {
@@ -29,6 +38,7 @@ public class BowItemMixin extends Item{
                 break;
             }
             else if(user.inventory.main[index] != null && user.inventory.main[index].itemId == Sandbox.torchArrow.id){
+                stack.damage(1, user);
                 user.inventory.remove(Sandbox.torchArrow.id);
                 world.playSound(user, "random.bow", 1.0F, 1.0F / (random.nextFloat() * 0.4F + 0.8F));
                 if (!world.isRemote) {
